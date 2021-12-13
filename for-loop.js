fc34dd0a246f9b1e9ee585b19429e76f5669919e
glitjch <t.jamesphan@gmail.com>
2021-12-13 20:10:44 +0000

    Adding all javascripting files to github.

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+let total = 0;
+const limit = 10;
+for (i = 0; i < limit; i++) {
+  //adding 1 to each loop
+  total += i;
+}
+console.log(total);
---
+++ looping-through-array.js
@@ -0,0 +1,6 @@
+const pets = ['cat', 'dog', 'rat'];
+for (i = 0; i < pets.length; i++) {
+  //pluraizing each pet
+  pets[i] += 's'; 
+}
+console.log(pets);
